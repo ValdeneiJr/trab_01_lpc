084cce7d0bc2dacd6abded9918d737fa29dbb27e
Valdenei Junior <vldsj.snf20@uea.edu.br>
2025-09-01 18:11:34 -0400

    minor changes

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from core import (
 
 
 if 'lang' not in st.session_state:
-
     st.session_state.lang = 'pt'
 
 with st.sidebar:
--- core.py
+++ core.py
@@ -4,15 +4,12 @@ import string
 POSSIBLE_CHARS = string.ascii_lowercase + string.digits + ' '
 
 def validate_input(phrase):
-    """Valida se a frase contém apenas os caracteres permitidos."""
     return all(char in POSSIBLE_CHARS for char in phrase)
 
 def generate_random_phrase(length):
-    """Gera uma frase aleatória com um determinado comprimento."""
     return ''.join(random.choice(POSSIBLE_CHARS) for _ in range(length))
 
 def mutate_phrase(phrase, mutation_rate=0.05):
-    """Muta aleatoriamente uma frase com base na taxa de mutação."""
     new_phrase = []
     for char in phrase:
         if random.random() < mutation_rate:
@@ -22,11 +19,9 @@ def mutate_phrase(phrase, mutation_rate=0.05):
     return ''.join(new_phrase)
 
 def reproduce(best_candidate, population_size=100, mutation_rate=0.05):
-    """Cria uma população de frases mutadas a partir do melhor candidato."""
     return [mutate_phrase(best_candidate, mutation_rate) for _ in range(population_size)]
 
 def select_best(population, target_phrase):
-    """Seleciona o melhor candidato da população com base na semelhança com o alvo."""
     best_score = -1
     best_candidate = ''
     for phrase in population:
